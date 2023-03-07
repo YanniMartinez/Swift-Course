827817c884033c342129ad1dc302c605c42efa1d
YanniMartinez <83842779+YanniMartinez@users.noreply.github.com>
2023-03-07 13:24:45 -0600

    Light project, first example app

---
+++ 2- Proyecto Guiado - Light/Light/Light/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by ios dev lab fi unam on 07/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var buttonPressed: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
