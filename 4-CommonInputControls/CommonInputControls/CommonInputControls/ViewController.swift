9bd720096bea4f8dab13830fe8c86f142adb2c3f
YanniMartinez <83842779+YanniMartinez@users.noreply.github.com>
2023-03-10 17:35:19 -0600

    4-Use of button UI

---
+++ 4-CommonInputControls/CommonInputControls/CommonInputControls/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CommonInputControls
+//
+//  Created by MacBook 26 on 10/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonTaped(_ sender: Any) {
+        print("Se presionó el botón.")
+    }
+    
+
+}
+
