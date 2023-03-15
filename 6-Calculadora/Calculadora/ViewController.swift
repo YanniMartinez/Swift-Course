69e079e13517972cef1cd6613b28886405e9ceae
YanniMartinez <83842779+YanniMartinez@users.noreply.github.com>
2023-03-15 14:01:08 -0600

    6-Creacion de esqueleto base de calculadora
    Dentro de esta aplicación y avance se verán conceptos como stacks que fungirán como grids los cuales nos permitan ubicar de forma simétrica botones para poder crear una calculadora la cual estaremos desarrollando durante el proyecto

---
+++ 6-Calculadora/Calculadora/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculadora
+//
+//  Created by MacBook 26 on 14/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
