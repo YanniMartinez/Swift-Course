42b6f71a340c31086459e1e2cd00761d5101c671
YanniMartinez <83842779+YanniMartinez@users.noreply.github.com>
2023-03-24 17:54:15 -0600

    Agregando un titulo en details

--- Swift-UI/LandMarks/LandmarkDetail.swift
+++ Swift-UI/LandMarks/LandmarkDetail.swift
@@ -9,39 +9,46 @@ import SwiftUI
 
 struct LandmarkDetail: View {
     var landmark: Landmark
-    var body: some View {
-        VStack{
-            //ScrollView es una vista
-            MapView(coordinate: landmark.locationCoordinate)
-                .ignoresSafeArea(edges: .top)//Ignora el area de seguridad
-                .frame(height: 300)
-            CircleImage()
-                .offset(y:-125)
-                .padding(.bottom,-130)
-            
-            VStack(alignment:.leading){//Puede heredar de vistas padre en este caso la alineacion
-                
-                
-                Text( landmark.name)
-                    .font(.title)
-                    .foregroundColor(.green)
-                HStack{
-                    Text(landmark.park)
+    
+        
+        
+        var body: some View {
+            ScrollView{
+                VStack{
+                    //ScrollView es una vista
+                    MapView(coordinate: landmark.locationCoordinate)
+                        .ignoresSafeArea(edges: .top)//Ignora el area de seguridad
+                        .frame(height: 300)
+                    CircleImage()
+                        .offset(y:-125)
+                        .padding(.bottom,-130)
+                    
+                    VStack(alignment:.leading){//Puede heredar de vistas padre en este caso la alineacion
                         
-                    Spacer()
-                    Text(landmark.state).font(.subheadline)
-                }.font(.subheadline) //Esto haria que se propage entre todos los elementos contenidos en la horizontal
-                    .foregroundColor(.secondary) //Aclaro un poco la letra del subtitulo
-                
-                Divider()// Genera una linea
-                Text(landmark.name)
-                    .font(.title2)
-                Text(landmark.description)
-                
-            }.padding(40)//Afecta toda la vista y lo que hay dentro de ella
-            Spacer()//Empuja la vista hacia arriba
+                        
+                        Text( landmark.name)
+                            .font(.title)
+                            .foregroundColor(.green)
+                        HStack{
+                            Text(landmark.park)
+                            
+                            Spacer()
+                            Text(landmark.state).font(.subheadline)
+                        }.font(.subheadline) //Esto haria que se propage entre todos los elementos contenidos en la horizontal
+                            .foregroundColor(.secondary) //Aclaro un poco la letra del subtitulo
+                        
+                        Divider()// Genera una linea
+                        Text(landmark.name)
+                            .font(.title2)
+                        Text(landmark.description)
+                        
+                    }.padding(40)//Afecta toda la vista y lo que hay dentro de ella
+                    Spacer()//Empuja la vista hacia arriba
+                }
+            }.navigationTitle(landmark.name)
+                .navigationBarTitleDisplayMode(.inline)
         }
-    }
+        
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
